3b7f1eca25d1706a1cdc316df04f129787f6c489
Tammi Jo <71151830+tanthony2020@users.noreply.github.com>
2020-09-16 18:20:10 -0400

    fix bug with followback

--- newfollowers.js
+++ newfollowers.js
@@ -9,11 +9,13 @@ var client = new Twitter({
     access_token_key: process.env.NEWFOLLOWER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.NEWFOLLOWER_ACCESS_TOKEN_SECRET
 });
-
+var newfollowers = new Array();
 console.log("starting MENTION");
 var filename="newfollowers.txt";
+
 if (fs.existsSync(filename))
 {
+    newFollowers = fs.readFileSync(filename).toString().split("\r\n");
     processNewFollowers();
 }
 var timer = setInterval(processNewFollowers, 3600000);// 900000); // 1 hour
@@ -23,8 +25,9 @@ function processNewFollowers()
     if (fs.existsSync(filename))
     {
         console.log("File exists goint to process");
-        ReadFile(filename);
-
+        newFollowers = fs.readFileSync(filename).toString().split("\r\n");
+        PostAppreciation(filename);
+        FollowBack(filename);
         fs.unlink(filename, function(err) {
             if (err) throw err;
             console.log("New Follower File deleted");
@@ -34,28 +37,39 @@ function processNewFollowers()
         console.log("File doesn't exist. Will check again in an hour");
     }
 }
-function ReadFile(filename)
+function PostAppreciation(filename)
     {
         console.log("starting to read file");
-        var newFollowers = fs.readFileSync(filename).toString().split("\n");
         for(var i=0;i<newFollowers.length;i++)
         {
-            client.get("users/lookup", {user_id:newFollowers[i]}, function(error, user, response) {
+            var id = newFollowers[i];
+            console.log("PostAppreciate ID: " + id);
+            client.get("users/lookup", {user_id:id}, function(error, user, response) {
                 if (error) console.log(error);
                 var _user = user[0].screen_name;
             
                 console.log(_user);
             
-                client.post('statuses/update', {status:"@"+_user + " Thanks for the follow."}, function (error, tweet, response) {
+                client.post('statuses/update', {status:"@"+_user + " Thanks for the follow. I followed back."}, function (error, tweet, response) {
                     if (error) console.log("There was an error sending post " + error);
                     console.log("Post sent to new follower @" + _user + " successfully");
                 });
-
-                // follow back
-                client.post("friendships/post", {user_id:newFollowers[i], follow:true}, function(error, user, response) {
-                    if (error) console.log(error);
-                    console.log("Follow back successful " + user.screen_name);
-                });            
             });
+        }
+    }
+    function FollowBack()
+    {
+        console.log("Starting followback");
+        
+        for(var i=0;i<newFollowers.length;i++)
+        {
+            var id = newFollowers[i];
+            console.log("FOLLOWBACK ID: " + id);
+                        
+            // follow back
+            client.post("friendships/create", {user_id:id, follow:true}, function(error, user, response) {
+                if (error) console.log(error);
+                console.log("Follow back successful " + user.screen_name);
+            });         
         }
     }
