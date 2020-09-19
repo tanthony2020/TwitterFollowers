7f0735682edf1e31c7e70074701a608ef5c0af8a
Tammi Jo <71151830+tanthony2020@users.noreply.github.com>
2020-09-19 11:42:40 -0400

    fixed bug
    There was a bug that was causing all followers to be duplicated in the new follower.txt and unfollow file

--- app.js
+++ app.js
@@ -35,6 +35,37 @@ function FileExists(filename)
 function ReadFile(filename)
 {
     followerList = fs.readFileSync(filename).toString().split("\n");
+    filtered = followerList.filter(Boolean);
+    followerList = filtered;
+}
+function RemoveLine(filename, userid)
+{
+    fs.readFile(filename, {encoding: 'utf8'}, function(err, data) {
+        if (err) throw error;
+    
+        var dataArray = data.split('\n'); // convert file data in an array
+        var searchKeyword = userid; // we are looking for a line, contains, key word  in the file
+        var lastIndex = -1; // let say, we have not found the keyword
+    
+        for (var index=0; index<dataArray.length; index++) {
+            if (dataArray[index].includes(searchKeyword)) { // check if a line contains the  keyword
+                lastIndex = index; // found a line includes a 'user1' keyword
+                break; 
+            }
+        }
+    
+        dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
+    
+        // UPDATE FILE WITH NEW DATA
+        // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
+        // THIS WILL REMOVE THE LINE CONTAINS userid IN YOUR FILE
+        var updatedData = dataArray.join('\n');
+        fs.writeFile(filename, updatedData, (err) => {
+            if (err) throw err;
+            console.log ('Successfully updated the current followers file and removing ' + userid);
+        });
+    
+    });
 }
 console.log("calling follows");
 var followerList = new Array();
@@ -53,12 +84,12 @@ function getFollowers() {
             // going to write to file instead
             // check to see if file exists
             // write the ides to a file
-            console.log("Writing lines to file");
+            console.log("Writing current followers to file");
             for(var i = 0; i< ids.ids.length;i++)
             {
                 WriteToFile(currentFollowersFilename, ids.ids[i] + "\n");
             }       
-
+            console.log("Done writing current followers to file ");
         } 
         // read from file and put in global list
         //followerList=ids.ids;
@@ -67,6 +98,7 @@ function getFollowers() {
             console.log("Reading file and adding values to the global object");
             ReadFile(currentFollowersFilename);
         }
+        console.log("Checking for unfollowers");
         // check to see if you have lost any followers
         for (var j = 0; j < followerList.length; j++) {
             var id = followerList[j];
@@ -74,14 +106,17 @@ function getFollowers() {
             var found = ids.ids.includes(id);
             
             if (!found) {
-                WriteToFile('unfollowers.txt', id+"\r\n");
+                WriteToFile('unfollowers.txt', id+"\n");
                 // you lost a follower
                 console.log("You lost a follower " + id);
                 // remove from list
                 var index = followerList.indexOf(id);
-                followerList.splice(index);
+                followerList.splice(index,1);
+                // remove line from current followers file too
+                RemoveLine(currentFollowersFilename, id);
             }
         }
+        console.log("Checking for new followers");
         // check for new followers
         for (var i = 0; i < ids.ids.length; i++) {
             var id = ids.ids[i];
